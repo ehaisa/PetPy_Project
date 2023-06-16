c9bdebdb4fc3955ba16eafe103edb3d93bce278c
ehaisa <isalaura1304@gmail.com>
2023-06-16 17:27:39 -0300

    creating 2nd version

---
+++ version2/menu2.py
@@ -0,0 +1,47 @@
+import os
+import platform
+
+system = platform.system()
+
+def limpar_tela():
+    if system == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def menuPrincipal():
+    limpar_tela()
+    print("=====================================")
+    print("             PETPY SHOP              ")
+    print("    A melhor opção para o seu pet    ")
+    print("=====================================")
+    print()
+    print("\t1 - Cadastre seu Pet")
+    print("\t2 - Nossos Serviços")
+    print("\t3 - Atendimento")
+    print("\t0 - Finalizar Programa")
+    print()
+    print("=====================================")
+    print()
+    opcao = input("O que você deseja acessar? ")
+    return opcao
+
+### PROGRAMA ###
+
+op1 = menuPrincipal()
+
+while op1 != "0":
+    if op1 == "1":
+        print("===     Pet Perfil     ===")
+        print("=== EM DESENVOLVIMENTO ===")
+    elif op1 == "2":
+        print("===    Serviços Pet    ===")
+        print("=== EM DESENVOLVIMENTO ===")
+    elif op1 == "3":
+        print("===    Agende Aqui     ===")
+        print("=== EM DESENVOLVIMENTO ===")
+    else:
+        print("===   OPÇÃO INVÁLIDA   ===")
+    input("Tecle ENTER para continuar")
+    op1 = menuPrincipal()
+print("Fim")
